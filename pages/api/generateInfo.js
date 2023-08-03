c5efd2843c315ac1b4cacaf5c9aeca0f6c2feeb6
Carlos Barroso <carlos.barroso@thoughtworks.com>
2023-08-03 12:55:32 +0200

    add recipe prompt to generateInfo request

--- pages/api/generateInfo.js
+++ pages/api/generateInfo.js
@@ -9,12 +9,13 @@
 // 7. export the generateInfo function as a module
 
 // import the Configuration class and the OpenAIApi class from the openai npm module
-const { Configuration, OpenAIApi } = require('openai');
+const { Configuration, OpenAIApi } = require("openai");
+const { recipePrompt } = require("../../data/recipe.json");
 
 // create a new configuration object that includes the api key and uses the Configuration class from the openai module
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-    basePath: 'https://api.openai.com/v1',
+  apiKey: process.env.OPENAI_API_KEY,
+  basePath: "https://api.openai.com/v1",
 });
 
 // create a new instance of the OpenAIApi class and pass in the configuration object
@@ -23,29 +24,35 @@ const openai = new OpenAIApi(configuration);
 // create an async function called generateInfo that accepts a request and response object as parameters
 
 const generateInfo = async (req, res) => {
-    // use try to make a request to the OpenAI completion api and return the response
-    try {
-        const response = await openai.completions.create({
-            engine: 'davinci',
-            prompt: 'This is a course about',
-            maxTokens: 60,
-            temperature: 0.9,
-            topP: 1,
-            presencePenalty: 0,
-            frequencyPenalty: 0,
-            bestOf: 1,
-            n: 1,
-            stream: false,
-            stop: ['\n'],
-        });
+  const { recipe } = req.body;
+  // use try to make a request to the OpenAI completion api and return the response
+  try {
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: `${recipePrompt}${recipe}` }],
+      max_tokens: 200,
+      temperature: 0,
+      n: 1,
+    });
+    const response = completion.data.choices[0].message.content;
 
-        // return the response
-        res.status(200).json(response.data);
-    } catch (error) {
-        // use catch to catch any errors and return the error include a message to the user
-        res.status(500).json({ error: error.message });
+    // return the response
+    return res.status(200).json({
+      success: true,
+      data: response,
+    });
+  } catch (error) {
+    if (error.response.status === 401) {
+      return res.status(401).json({
+        error: "Please provide a valid API key.",
+      });
     }
-}
+    return res.status(500).json({
+      error:
+        "An error occurred while generating recipe information. Please try again later.",
+    });
+  }
+};
 
 // export the generateInfo function as a module
 module.exports = generateInfo;
